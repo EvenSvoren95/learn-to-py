57c3d0df32b7b006a9a4ba74cba4b6bc4a4dbd35
Even Svoren <even.svoren@gmail.com>
2024-05-08 12:14:21 +0200

    update1.2

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import csv
 class CSVApp:
     def __init__(self, root):
         self.root = root
-        self.root.title("CSV Data App")
+        self.root.title("CSV new App")
         #test
 
         # Create Canvas with Scrollbar
